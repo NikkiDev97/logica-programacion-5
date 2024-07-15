30b5827a7cfab8580e57017b88d3f11107ac28d2
Nicole Lorena Rojas Ramirez <nicole.rojas@genstudents.org>
2024-07-14 23:55:31 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+  public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    Map<String, String> diccionario = new HashMap<>();
+
+    diccionario.put("clase", "class");
+    diccionario.put("objeto", "object");
+    diccionario.put("método", "method");
+    diccionario.put("función", "function");
+    diccionario.put("atributo", "attribute");
+    diccionario.put("herencia", "inheritance");
+    diccionario.put("polimorfismo", "polymorphism");
+    diccionario.put("interfaz", "interface");
+    diccionario.put("sobrecarga", "overload");
+    diccionario.put("sobrescritura", "override");
+    diccionario.put("algoritmo", "algorithm");
+    diccionario.put("bucle", "loop");
+    diccionario.put("condicional", "conditional");
+    diccionario.put("base de datos", "database");
+    diccionario.put("servidor", "server");
+    diccionario.put("biblioteca", "library");
+    diccionario.put("despliegue", "deployment");
+    diccionario.put("entorno", "environment");
+    diccionario.put("desarrollo", "development");
+    diccionario.put("rendimiento", "performance");
+
+    Random random = new Random();
+
+    List<String> palabrasEspannol = new ArrayList<>(diccionario.keySet());
+    List<String> palabrasSeleccionadas = new ArrayList<>();
+
+    for (int i = 0; i < 5; i++) {
+      int randomIndex = random.nextInt(palabrasEspannol.size());
+      palabrasSeleccionadas.add(palabrasEspannol.get(randomIndex));
+      palabrasEspannol.remove(randomIndex);
     }
+
+    Scanner scanner = new Scanner(System.in);
+    int respuestasCorrectas = 0;
+    int respuestasIncorrectas = 0;
+
+    for (String palabra : palabrasSeleccionadas) {
+      System.out.println("Traduce la palabra: " + palabra);
+      String traduccion = scanner.nextLine();
+
+      if (diccionario.get(palabra).equalsIgnoreCase(traduccion)) {
+        respuestasCorrectas++;
+      } else {
+        respuestasIncorrectas++;
+      }
+    }
+    System.out.println("Respuestas correctas: " + respuestasCorrectas);
+    System.out.println("Respuestas incorrectas: " + respuestasIncorrectas);
+
+    scanner.close();
+  }
 }
